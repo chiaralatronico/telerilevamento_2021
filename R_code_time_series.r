9a4d559b35f8a7e5d81b845f36d6dfb9843adcec
chiaralatronico <80513546+chiaralatronico@users.noreply.github.com>
2021-04-09 13:01:15 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,1 +1,11 @@
+# Time series analysis
+# Greenland increase of temperature 
+# Data and code from Emanuela Cosma
+
+#richiamo il pacchetto raster
+library(raster)
+
+#specifico il nuovo percorso di salvataggio dei dati
+setwd("D:/lab/Greenland")
+
 
