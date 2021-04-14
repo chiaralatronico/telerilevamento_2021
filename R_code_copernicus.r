ad6273fc446aeb1ab8b2c0f9782736bb39ff2e53
chiaralatronico <80513546+chiaralatronico@users.noreply.github.com>
2021-04-14 12:29:50 +0200

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,15 @@
+#R_code_copernicus.r
+#Visualizing Copernicus data
+
+#installo il pacchetto ncdf4
+install.packages("ncdf4")
+
+#richiamo il pacchetto ncdf4
+library(ncdf4)
+
+#richiamo il pacchetto raster
+library(raster)
+
+#specifico il percorso di salvatggio dei dati scaricati
+setwd("D:/lab/")
+
