953621ea1b11edf7f6768549a9096531adec355a
chiaralatronico <80513546+chiaralatronico@users.noreply.github.com>
2021-05-03 17:54:08 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,8 +3,24 @@
 #specifico il percorso di salvataggio dei dati
 setwd("D:/lab/")
 
-#richiamo il pacchetto knitr
+#richiamo il pacchetto knitr per creare report
 library(knitr)
 
-#utilizzo la funzione stitch
+#edito e copio il codie "time series" sulla Groenlandia, lo incollo su un editor di testi e lo salvo come "R_code_greenland.txt"
+
+#utilizzo la funzione stitch per la creazione di report automatici
+stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+#errore: manca il pacchetto tinytex
+
+#installo il pacchetto tinyhtex
+install.packages("tinytex")
+
+#ripeto la funzione stitch. Crea un file .tex che però non riesce a compilare.
+#risolvo con:
+tinytex::install_tinytex()
+tinytex::tImgr_update()
+
+#rilancio la funzione stitch e mi crea il pdf richiesto
+stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
