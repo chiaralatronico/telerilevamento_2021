efd5a6469fbde7be1b34342158c1ec46da40c64f
chiaralatronico <80513546+chiaralatronico@users.noreply.github.com>
2021-05-04 15:13:10 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -3,3 +3,39 @@
 #specifico il percorso di salvataggio dei dati
 setwd("D:/lab/")
 
+#richiamo il pacchetto raster
+library(raster)
+
+#richiamo il pacchetto RStoolbox
+library(RStoolbox)
+
+#salvo l'immagine riferita al sole e ai suoi diversi livelli energetici
+#utilizzo la funzione brick per importarla in R e la rinomino "so" (solar orbiter)
+so<-brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+#stampo l'immagine in RGB
+plotRGB(so, 1, 2, 3, stretch="Lin")
+
+#per classificare tale immagine utilzzo la funzione unsuperClass (unsupervisored classification), con 3 classi. Rinomino soc (solar orbiter classified)
+soc<-unsuperClass(so, nClasses=3)
+
+#plot dell'immagine (solo la mappa)
+plot(soc$map)
+
+#riclassifico l'immagine con 20 venti classi
+soc20<-unsuperClass(so, nClasses=20)
+
+#plot della nuova classificazione (solo mappa)
+plot(soc20$map)
+
+#scarico nuova immagine del sole dal sito dell'Esa-Solar Orbiter; la sinomino "sun"
+#importo l'immagine in R con la funzione brick
+sun<-brick("sun.png")
+
+#classfico l'immagine con 3 classi
+sunc<-unsuperClass(sun, nClasses=3)
+
+#stampo l'immagine (solo la mappa)
+plot(sunc$map)
+
+
