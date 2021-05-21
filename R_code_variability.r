f9ac0a48228eb4e8a14ab4faf54ed66c74bf9c90
chiaralatronico <80513546+chiaralatronico@users.noreply.github.com>
2021-05-21 10:51:18 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -67,3 +67,69 @@ sentpca
 
 summary(sentpca$model)
 the first pca contiene il ...
+
+
+setwd("D:/lab/")
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(gridExtra)
+sent<-brick("sentinel.png")
+sent <- brick("sentinel.png")
+sentpca <- rasterPCA(sent)
+plot(sentpca$map)
+sentpca
+summary(sentpca$model) 
+#67.qualcosa% di variabilità spiegato attorno alla prima componente
+
+#focal 
+
+sentpca$map
+#la prima componente si chiama pc1
+
+pc1 <- sentpca$map$PC1
+pc1sd5<-focal(pc1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100)
+plot(pc1sd5, col=clsd)
+
+#su virtuale pezzo di codice 7x7
+#lo salvo in lab in blocknotes con nome source.txt (in realtà va ftto save link as)
+#in r con la funzione source richiamo il codice (carica codici dall'esterno)
+source("source.txt")
+ 
+install.packages("viridis")
+library(viridis)
+
+#importo altro codice
+source("source_ggplot.r")
+
+#faccio una nuova finestra vuota (con il + aggiungo dei blocchi)
+ggplot()+
+
+#le estetiche sono cosa plotto (sono le mapping)
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer))
+
+ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis()
+
+#https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+p1<-ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis()  +
+ggtitle("Standard deviation of PC1 by viridis colour scale")
+
+p2<-ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option = "magma")  +
+ggtitle("Standard deviation of PC1 by magma colour scale")
+#vengono fuori molto bene le parti con una alta dev.st
+
+p3<-ggplot() +
+geom_raster(pc1sd5, mapping = aes(x = x, y = y, fill = layer)) +
+scale_fill_viridis(option = "inferno")  +
+ggtitle("Standard deviation of PC1 by inferno colour scale")
+
+#con grid.arrange metto insieme più mappe di ggplot
+grid.arrange(p1, p2, p3, nrow=3)
+#è il metodo con il quale il prof ci ha passato il codice pieno di mappe
